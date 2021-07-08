b89153d2cbee6a17799b1a18c74ee18b96f6d763
gatsbytrial <87015110+gatsbytrial@users.noreply.github.com>
2021-07-08 17:25:06 +0100

    Update page-2.js
    Just seeing what happens with this.

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,13 +4,36 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+class SomePage extends React.Component {
 
-export default SecondPage
+    constructor(props) {
+        super(props);
+
+        this.state = {compOneData:''};
+
+    }
+
+    componentDidMount(){
+        this.loadData();
+    }
+
+    loadData(){
+        fetch("https://rg-homeservecomsc-reg-uks-001-single.azurewebsites.net/sitecore/api/ssc/item/04ed4d48-376a-4586-a06a-936615a52499")
+            .then(result=>result.json())
+            .then(result=>this.setState({compOneData: result[0].GaPageSubcategory}); 
+    }
+
+
+    render(){
+
+        return (
+
+            <div>
+            <ComponentOne data={this.state.compOneData} />
+            </div>
+        );
+
+    }
+}
+
+export default SomePage;
